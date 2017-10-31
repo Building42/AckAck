3ed3e4e05b69d0766e852b5ba7337937d4f0c014
Sommer Panage <sommer@panage.org>
2017-10-31 15:05:18 -0700

    support markdown license files

--- ackack.py
+++ ackack.py
@@ -194,7 +194,7 @@ def generate(input_folder, output_folder, max_depth, clean_up, quiet):
                 continue
 
             # Look for license files
-            if file_name.endswith('LICENSE') or file_name.endswith('LICENSE.txt'):
+            if file_name.endswith('LICENSE') or file_name.endswith('LICENSE.txt') or file_name.endswith('LICENSE.md'):
                 license_path = os.path.join(root, file_name)
 
                 # We found a license
